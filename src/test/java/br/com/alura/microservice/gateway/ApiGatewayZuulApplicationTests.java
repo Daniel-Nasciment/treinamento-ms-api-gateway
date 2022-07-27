a46ab98fddd9828c549a9cda7f9346835f76f69c
Daniel-Nasciment <daniel.fecaf@outlook.com>
2022-07-27 15:51:15 -0300

    Projeto Base

---
+++ src/test/java/br/com/alura/microservice/gateway/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.microservice.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
